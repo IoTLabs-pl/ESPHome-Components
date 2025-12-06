539bb2d10ba8d596e55b3eea9699d3bc1107e115
Kuba Sawulski <kuba@sawul.ski>
2025-12-06 23:07:16 +0100

    chore: run formatter over cpp codebase

--- components/wmbus_common/component.cpp
+++ components/wmbus_common/component.cpp
@@ -5,32 +5,28 @@
 #include "esphome/core/defines.h"
 #include "esphome/core/log.h"
 
-namespace esphome
-{
-    namespace wmbus_common
-    {
-        static const char *TAG = "wmbus_common";
+namespace esphome {
+namespace wmbus_common {
+static const char *TAG = "wmbus_common";
 
-        std::vector<std::string> sorted_driver_names()
-        {
-            std::vector<std::string> driver_names;
-            driver_names.reserve(allDrivers().size());
-            for (auto &driver : allDrivers())
-                driver_names.push_back(driver->name().str());
-            std::sort(driver_names.begin(), driver_names.end());
-            return driver_names;
-        }
-
-        const std::vector<std::string> driver_names = std::move(sorted_driver_names());
-
-        void WMBusCommon::dump_config()
-        {
-            ESP_LOGCONFIG(TAG, "wM-Bus Component:");
-            ESP_LOGCONFIG(TAG, "  wmbusmeters version: %s", WMBUSMETERS_TAG);
-            ESP_LOGCONFIG(TAG, "  Loaded drivers:");
-            for (auto &driver : driver_names)
-                ESP_LOGCONFIG(TAG, ("    - " + driver).c_str());
-        }
-
-    }
+std::vector<std::string> sorted_driver_names() {
+  std::vector<std::string> driver_names;
+  driver_names.reserve(allDrivers().size());
+  for (auto &driver : allDrivers())
+    driver_names.push_back(driver->name().str());
+  std::sort(driver_names.begin(), driver_names.end());
+  return driver_names;
 }
+
+const std::vector<std::string> driver_names = std::move(sorted_driver_names());
+
+void WMBusCommon::dump_config() {
+  ESP_LOGCONFIG(TAG, "wM-Bus Component:");
+  ESP_LOGCONFIG(TAG, "  wmbusmeters version: %s", WMBUSMETERS_TAG);
+  ESP_LOGCONFIG(TAG, "  Loaded drivers:");
+  for (auto &driver : driver_names)
+    ESP_LOGCONFIG(TAG, ("    - " + driver).c_str());
+}
+
+}  // namespace wmbus_common
+}  // namespace esphome
